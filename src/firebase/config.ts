3052d959a14e488705f9137f170c3ae2b44eee86
Andries Liebenberg <create@an3s.info>
2025-10-30 17:23:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2438084015-e2683",
+  "appId": "1:958683911757:web:fa823df82bf037c8bc6e04",
+  "apiKey": "AIzaSyDlKqlyFsxxSJ9zHthe1rUnwgR_Fbj4XnI",
+  "authDomain": "studio-2438084015-e2683.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "958683911757"
+};
